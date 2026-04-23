7a5ca6d931f0b4e19dabcc613a38350baf67e58a
Burak Morina <Bumo74774@edu.ucl.dk>
2026-04-23 09:10:48 +0200

    Add project files.

---
+++ BookRight.UseCases.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace BookRight.UseCases.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
