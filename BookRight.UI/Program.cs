3467df5123f2a86665d5b3307ca61fba61ff0637
Burak Morina <Bumo74774@edu.ucl.dk>
2026-04-30 08:51:41 +0200

    .

--- BookRight.UI/Program.cs
+++ BookRight.UI/Program.cs
@@ -1,13 +1,9 @@
 using BookRight.UI.Components;
 using BookRight.Infrastructure.DependencyInjection;
-using BookRight.UseCases.DependencyInjection;
-using BookRight.Facade.DependencyInjection;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddInfrastructure(builder.Configuration);
-builder.Services.AddUseCases();
-builder.Services.AddFacade();
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
