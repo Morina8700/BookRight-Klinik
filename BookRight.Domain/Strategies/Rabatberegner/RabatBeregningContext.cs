1dc2d72010658e2449a71eb67550d2e51f9d8802
Burak Morina <Bumo74774@edu.ucl.dk>
2026-04-30 10:39:51 +0200

    Work in progress

---
+++ BookRight.Domain/Strategies/Rabatberegner/RabatBeregningContext.cs
@@ -0,0 +1,20 @@
+﻿using BookRight.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookRight.Domain.Strategies.Rabatberegner
+{
+    public record RabatBeregningContext(
+    
+        decimal PrisUdenRabat,
+        DateOnly BookingDato,
+        DateOnly KundeFoedselsdato,
+        LoyalitetsNiveau LoyalitetsNiveau,
+        bool FoedselsdagsrabatBrugt,
+        IReadOnlyCollection<string> Behandlingstyper,
+        IReadOnlyCollection<Kampagne> AktivKampagner
+    );
+
+    
+}
---
+++ BookRight.Domain/Strategies/Rabatberegner/RabatResultat.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookRight.Domain.Strategies.Rabatberegner
+{
+    public record RabatResultat(
+        string RabatNavn,
+        decimal RabatProcent,
+        decimal PrisUdenRabat,
+        decimal PrisMedRabat
+
+
+        );
+}
