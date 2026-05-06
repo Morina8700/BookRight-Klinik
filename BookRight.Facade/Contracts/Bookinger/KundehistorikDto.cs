207c36099e45bd957f0b8070a68e14f3ee9cf515
akhelyesa <elyesaekici9@gmail.com>
2026-05-06 11:25:22 +0200

    Opdateret HentKundehistorik funktionalitet i BookingFacade og tilføjet nye metoder i BookingRepository. Justeret ServiceCollectionExtension for at inkludere HentKundehistorikHandler.

---
+++ BookRight.Facade/Contracts/Bookinger/KundehistorikDto.cs
@@ -0,0 +1,13 @@
+namespace BookRight.Facade.Contracts.Bookinger
+{
+    public class KundehistorikDto
+    {
+        public Guid BookingId { get; set; }
+        public DateTime StartTid { get; set; }
+        public DateTime SlutTid { get; set; }
+        public string KlinikNavn { get; set; } = string.Empty;
+        public string BehandlerNavn { get; set; } = string.Empty;
+        public string BehandlingstypeNavn { get; set; } = string.Empty;
+        public string Status { get; set; } = string.Empty;
+    }
+}
